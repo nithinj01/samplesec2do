4b5d88ea5e7c5001ecbabcf6b9f0c6fcbc1f1422
Nithin J <47292784+nithinj01@users.noreply.github.com>
2022-04-10 17:37:45 +0530

    Update ArticleController.kt

--- src/main/kotlin/com/sec2do/coding/challenges/mapping/controllers/ArticleController.kt
+++ src/main/kotlin/com/sec2do/coding/challenges/mapping/controllers/ArticleController.kt
@@ -1,6 +1,8 @@
 package com.sec2do.coding.challenges.mapping.controllers
 import com.google.gson.Gson
 import com.google.gson.GsonBuilder
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import com.sec2do.coding.challenges.mapping.models.dto.ArticleDto
 import com.sec2do.coding.challenges.mapping.services.ArticleService
 import org.springframework.web.bind.annotation.*
@@ -11,10 +13,20 @@ class ArticleController(
     private val articleService: ArticleService
 ) {
     @GetMapping
-    fun list(): List<ArticleDto> = articleService.list()
+    fun list(): List<ArticleDto> {
+        val dtoList: List<ArticleDto> =articleService.list()
+        val gson = Gson()
+        val jsonlist=gson.toJson(dtoList)
+        return jsonlist
+    }
 
     @GetMapping("/{id}")
-    fun details(@PathVariable id: Long): ArticleDto = articleService.articleForId(id)
+    fun details(@PathVariable(value = "id") articleId: Long): ResponseEntity<ArticleDto> {
+        return articleService.articleForId(articleId).map { article ->
+            ResponseEntity.ok(article)
+        }.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).body(errors))
+    }
+
 
     @PostMapping
     fun create(@RequestBody articleDto: ArticleDto): ArticleDto = articleService.create(articleDto)
